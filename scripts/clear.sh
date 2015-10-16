69370484140dac729da3d3dc4969c301f1031c57
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2015-10-16 17:39:01 +0800

    clear.sh doesn't rm README.md

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
  find . -maxdepth 1 \
-         -not -name "*.sh" | xargs rm
+         -not -name "*.sh" -not -name "*.md" | xargs rm
 #        -not -name "*call.c" \
 #        -not -name "*termination.c" \
 #        -not -name "*ground.c" \
 rm -rf output
+rm -rf sideProducts
