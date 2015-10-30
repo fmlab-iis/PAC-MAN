f6d424a2e93525e079d876abccadb467329d28b7
usefulalgorithm <usefulalgorithm@gmail.com>
2015-10-30 20:55:12 +0800

    Change build script according to CPAchecker version

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd CPAchecker; ant; cd ..; cd crest-0.1.2/cil; ./configure; make; cd ../src; make; cd ../..; cd ocaml; make; cd ..; cd genWitness; make; cd ..; cd array; make; cd ..
+cd CPAchecker-1.4-svn; ant; cd ..; cd crest-0.1.2/cil; ./configure; make; cd ../src; make; cd ../..; cd ocaml; make; cd ..; cd genWitness; make; cd ..;
