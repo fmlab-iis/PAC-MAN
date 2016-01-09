d538f7ab05ce9c561a069d77491a37e278c7841f
usefulalgorithm <usefulalgorithm@gmail.com>
2016-01-09 19:51:09 +0800

    Generate not-as-powerful witnesses as the original is faulty

--- scripts/gen_witness.sh
+++ scripts/gen_witness.sh
@@ -4,7 +4,12 @@ BIN=${BASE}/scripts/cpa.sh
 CONF=${BASE}/config/generateWitness.properties
 SPEC=${BASE}/config/specification/sv-comp-reachability.spc
 date1=$(date +"%s")
-${BIN} -heap 10000M -config ${CONF} -spec ${SPEC} -noout $*
+#${BIN} -heap 10000M -config ${CONF} -spec ${SPEC} -noout $*
+if [ -d output ]; then
+  rm -rf output
+fi
+mkdir output
+cp template.graphml output/witness.graphml
 date2=$(date +"%s")
 diff=$(($date2-$date1))
 echo -e "\n*** Witness Generation: $(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
